47dd4a884fa1cfc12c9fc691bcf0e3b45f9f25c2
Josh Yim <jyim@lcs.on.ca>
2024-01-11 14:05:59 -0500

    Initial Commit

---
+++ Home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Home
+//
+//  Created by junxi Yim on 2024/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by junxi Yim on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
